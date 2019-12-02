d5970a242e01a6274b352d8a1e2f5ec2a6d80d2c
Kalleo Pinheiro <windson.rocha@zup.com.br>
2019-12-02 17:48:42 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_, res) => {
+  res.json({ message: 'Working' }).end();
+});
+
+app.listen(3000);
