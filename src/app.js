9b022d0248b9021ba82e499dbb74315db271a70c
Kalleo Pinheiro <kalleopinheiro@hotmail.com>
2020-02-23 11:31:49 -0300

    improvement: 🚀 Remove cross-env and cookie-session

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const helmet = require('helmet');
-const session = require('cookie-session');
 const routes = require('./app/routes');
 require('dotenv').config();
 
@@ -8,17 +7,6 @@ require('dotenv').config();
   const app = express();
   app.use(helmet());
   app.use(express.json({ limit: '300kb' }));
-  app.use(
-    session({
-      secret: process.env.SECRET_KEY,
-      name: process.env.API_NAME,
-      cookie: {
-        httpOnly: true,
-        secure: true,
-        maxAge: 60000 * 60 * 24,
-      },
-    })
-  );
 
   // Load Routes
   app.use(routes);
